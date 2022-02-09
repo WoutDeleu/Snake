cde52398268f5f053821c979c60cbab14c40824b
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-02-09 13:56:14 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+///SOURCE:::: https://www.google.com/search?q=programming+snake+in+java&oq=programming+snake+&aqs=chrome.1.69i57j0i512l3j0i22i30l3j69i60.4440j0j1&sourceid=chrome&ie=UTF-8#kpvalbx=_zb8CYv3IJpGAkwXCnJygDw22
+
+public class Main {
+    public static void main(String[] args) {
+        new GameFrame();
+
+    }
+}
